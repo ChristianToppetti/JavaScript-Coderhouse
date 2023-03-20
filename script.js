ac223ea0d16a7be1c51648e161c93882da4130a0
toppetti_christian <toppettichristian@gmail.com>
2023-03-20 14:24:06 -0300

    Rehecho las array type, colors y sizes con objetos

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const productsArray = []
 
 const productTypes = [
-    REMERA = "REMERA", 
-    VESTIDO = "VESTIDO", 
-    CALZADO = "CALZADO"
-    ]
+    {id: "REMERA"},
+    {id: "VESTIDO"}, 
+    {id: "CALZADO"}
+]
 
 const productColors = [
-    RED = "ROJO", 
-    BLUE = "AZUL", 
-    BLACK = "NEGRO",
-    WHITE = "BLANCO"
-    ]
+    {id: "ROJO"}, 
+    {id: "AZUL"}, 
+    {id: "NEGRO"},
+    {id: "BLANCO"}
+]
 
 const productSizes = [
-    S = "S", 
-    M = "M", 
-    L = "L",
-    X = "X",
-    XL = "XL",
-    XLL = "XLL"
-    ]
+    {id: "S"}, 
+    {id: "M"}, 
+    {id: "L"},
+    {id: "X"},
+    {id: "XL"},
+    {id: "XLL"}
+]
 
 class Product {
     constructor(name, type, price, sizes, colors) {
@@ -67,47 +67,47 @@ const createMainMenu = (objArray) => {
 const createFilterMenu = (array) => {
     let string = "Eliga una opcion:\n\n"
 
-    array.forEach((e, i) => string += `${i+1}. ${e}\n`)
+    array.forEach((e, i) => string += `${i+1}. ${e.id}\n`)
 
     return string
 }
 
 const initSimulator = () => {
-    let sizes = [L, X, XL]
-    let colors = [WHITE, BLACK]
-    addProduct("Remera Lisa", REMERA, 1234.1, sizes, colors)
+    let sizes = [productSizes[0].id, productSizes[1].id, productSizes[2].id]
+    let colors = [productColors[0].id, productColors[1].id]
+    addProduct("Remera Lisa", productTypes[0].id, 1234.1, sizes, colors)
 
-    sizes = [X, XL]
-    colors = [WHITE, BLACK]
-    addProduct("Remera Deportiva", REMERA, 1234.2, sizes, colors)
+    sizes = [productSizes[3].id, productSizes[4].id, productSizes[5].id]
+    colors = [productColors[2].id, productColors[3].id]
+    addProduct("Remera Deportiva", productTypes[0].id, 1234.2, sizes, colors)
 
-    sizes = [L, X, XL, XLL]
-    colors = [BLUE, BLACK, WHITE]
-    addProduct("Remera Rayada", REMERA, 1234.3, sizes, colors)
+    sizes = [productSizes[2].id, productSizes[1].id, productSizes[4].id]
+    colors = [productColors[0].id, productColors[1].id, productColors[2].id]
+    addProduct("Remera Rayada", productTypes[0].id, 1234.3, sizes, colors)
 
-    sizes = [S, M, X, XL, XLL]
-    colors = [BLACK]
-    addProduct("Vestido Corto", VESTIDO, 2345.4, sizes, colors)
+    sizes = [productSizes[3].id, productSizes[4].id, productSizes[5].id]
+    colors = [productColors[3].id]
+    addProduct("Vestido Corto", productTypes[1].id, 2345.4, sizes, colors)
 
-    sizes = [S, L, XLL]
-    colors = [RED, BLUE, BLACK, WHITE]
-    addProduct("Vestido Largo", VESTIDO, 2345.5, sizes, colors)
+    sizes = [productSizes[2].id, productSizes[3].id, productSizes[4].id]
+    colors = [productColors[0].id, productColors[1].id, productColors[2].id, productColors[3].id]
+    addProduct("Vestido Largo", productTypes[1].id, 2345.5, sizes, colors)
 
-    sizes = [M, L, X, XL]
-    colors = [BLACK, WHITE]
-    addProduct("Vestido de Lino", VESTIDO, 2345.6, sizes, colors)
+    sizes = [productSizes[0].id, productSizes[1].id, productSizes[2].id]
+    colors = [productColors[1].id, productColors[3].id]
+    addProduct("Vestido de Lino", productTypes[1].id, 2345.6, sizes, colors)
 
-    sizes = [L, X, XL]
-    colors = [WHITE]
-    addProduct("Zapatillas Nike", CALZADO, 3456.7, sizes, colors)
+    sizes = [productSizes[1].id, productSizes[2].id, productSizes[3].id]
+    colors = [productColors[1].id]
+    addProduct("Zapatillas Nike", productTypes[2].id, 3456.7, sizes, colors)
 
-    sizes = [L, X, XL]
-    colors = [BLUE]
-    addProduct("Crocs Classic", CALZADO, 3456.8, sizes, colors)
+    sizes = [productSizes[0].id, productSizes[1].id, productSizes[2].id]
+    colors = [productColors[1].id]
+    addProduct("Crocs Classic", productTypes[2].id, 3456.8, sizes, colors)
 
-    sizes = [L, X, XL]
-    colors = [BLACK]
-    addProduct("Borcegos Cuero", CALZADO, 3456.9, sizes, colors)
+    sizes = [productSizes[3].id, productSizes[4].id, productSizes[5].id]
+    colors = [productColors[2].id]
+    addProduct("Borcegos Cuero", productTypes[2].id, 3456.9, sizes, colors)
 }
 
 const startSimulator = (menu, objArray) => {
@@ -118,7 +118,7 @@ const startSimulator = (menu, objArray) => {
             menu = createFilterMenu(productTypes)
             key = parseInt(prompt(menu)-1)
 
-            objArray = filterProduct(productTypes[key], "type")
+            objArray = filterProduct(productTypes[key].id, "type")
 
             menu = createMainMenu(objArray)
             startSimulator(menu, objArray)
@@ -128,7 +128,7 @@ const startSimulator = (menu, objArray) => {
             menu = createFilterMenu(productColors)
             key = parseInt(prompt(menu)-1)
             
-            objArray = filterProduct(productColors[key], "colors")
+            objArray = filterProduct(productColors[key].id, "colors")
 
             menu = createMainMenu(objArray)
             startSimulator(menu, objArray)
@@ -138,7 +138,7 @@ const startSimulator = (menu, objArray) => {
             menu = createFilterMenu(productSizes)
             key = parseInt(prompt(menu)-1)
             
-            objArray = filterProduct(productSizes[key], "sizes")
+            objArray = filterProduct(productSizes[key].id, "sizes")
 
             menu = createMainMenu(objArray)
             startSimulator(menu, objArray)
